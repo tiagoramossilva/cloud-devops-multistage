44ce796a7a1d97ed1c0dffb83321127a6c809e6d
Tiago Ramos Silva <116301732+tiagoramossilva@users.noreply.github.com>
2026-05-08 15:28:53 +0200

    test pipeline

--- main.tf
+++ main.tf
@@ -2,6 +2,9 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+# -------------------------
+# SECURITY GROUP
+# -------------------------
 resource "aws_security_group" "app_sg" {
   name = "app-sg"
 
@@ -27,24 +30,80 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
+# -------------------------
+# IAM ROLE (ECR ACCESS)
+# -------------------------
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-ecr-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_policy" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2_profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+# -------------------------
+# EC2 INSTANCE
+# -------------------------
 resource "aws_instance" "app" {
   ami           = "ami-0f7804991cb8f07c4"
   instance_type = "t3.micro"
 
   vpc_security_group_ids = [aws_security_group.app_sg.id]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              apt update -y
-              apt install -y docker.io
-              systemctl start docker
-              systemctl enable docker
-              usermod -aG docker ubuntu
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
-              docker run -d -p 5000:5000 481719141129.dkr.ecr.eu-central-1.amazonaws.com/flask-app:latest
-              EOF
+user_data = <<-EOF
+        #!/bin/bash
+        exec > /var/log/user-data.log 2>&1
+        set -x
+
+        apt update -y
+        apt install -y docker.io awscli
+
+        systemctl start docker
+        systemctl enable docker
+
+        usermod -aG docker ubuntu
+
+        aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 481719141129.dkr.ecr.eu-central-1.amazonaws.com
+
+        docker pull 481719141129.dkr.ecr.eu-central-1.amazonaws.com/flask-app:latest
+
+        docker stop app || true
+        docker rm app || true
+
+        docker run -d -p 5000:5000 --name app 481719141129.dkr.ecr.eu-central-1.amazonaws.com/flask-app:latest
+        EOF
 
   tags = {
     Name = "ci-cd-multistage"
   }
+}
+
+# -------------------------
+# OUTPUTS
+# -------------------------
+output "instance_public_ip" {
+  value = aws_instance.app.public_ip
+}
+
+output "application_url" {
+  value = "http://${aws_instance.app.public_ip}:5000"
 }
